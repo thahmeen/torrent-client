4a1de07d6fc3398603a2b5f41fa9008625c74073
mona456 <mona.thahmeen@cresensolutions.com>
2024-06-02 21:04:41 +0530

    open torrent file test.torrent and get the tracker url

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 const fs = require('fs');
 //readFileSync is the best method to read the content of the file
 //It returns buffer whihc is a sequence raw byte
-const torrent = fs.readFileSync('test.torrent');
-console.log(torrent.toString('utf8'));
+// const torrent = fs.readFileSync('test.torrent');
+
+// data serialization format, and I don’t think I’ve seen it used anywhere outside of torrent files.
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('test.torrent'));
+const trackerUrl = torrent.announce.toString('utf8');
+console.log(trackerUrl);
